866fb16a6cb755cbd4aebd9fdc6068fd4e4a0632
Tom Howland <tomhowland9@gmail.com>
2023-06-24 16:50:51 -0700

    Fix

--- src/lib/is-on-production.ts
+++ src/lib/is-on-production.ts
@@ -3,6 +3,9 @@ import type { Rule } from "./storage";
 
 export function isOnProduction(url: URL, rules: Rule[]) {
     for (const rule of rules) {
+        if (!rule.enabled) {
+            continue;
+        }
         switch (rule.type) {
             case 'starts with':
                 if (url.toString().startsWith(rule.value)) {
